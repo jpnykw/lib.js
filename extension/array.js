9c82773d31e630251b2131fb95fbdf2d4314760c
Haruto Hirakawa <31243896+JPNYKW@users.noreply.github.com>
2019-06-04 12:15:46 +0900

    Update array.js

--- extension/array.js
+++ extension/array.js
@@ -1,3 +1,7 @@
+Array.prototype.last = function (getIndex) {
+  return getIndex ? this.length - 1 : this[this.length - 1];
+}
+
 Array.prototype.random = function (getIndex) {
   const index = (Math.random() * this.length) >> 0;
   return getIndex ? index : this[index];
