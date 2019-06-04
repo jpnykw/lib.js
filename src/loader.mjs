cf05f71d7cc59d164dfe583e81870b2282f3505e
Haruto Hirakawa <31243896+JPNYKW@users.noreply.github.com>
2019-06-04 15:29:07 +0900

    Update loader.mjs

--- src/loader.mjs
+++ src/loader.mjs
@@ -1,5 +1,11 @@
 import {init as initArray} from './extension/array.mjs';
+import {init as initNumber} from './extension/number.mjs';
+import {init as initObject} from './extension/object.mjs';
+import {init as initString} from './extension/string.mjs';
 
 export const init = () => {
 	initArray();
+	initNumber();
+	initObject();
+	initString();
 };
